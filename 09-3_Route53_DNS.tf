03a0586486c0a0d9031afdb396d097b9d9e1e5ba
ceool <p1026jh@naver.com>
2021-05-31 16:23:52 +0900

    Add A root_domain

--- 09-3_Route53_DNS.tf
+++ 09-3_Route53_DNS.tf
@@ -8,6 +8,8 @@ resource "aws_route53_zone" "domain_01" {
   name = "example.com"
 }
 */
+
+# root domain (www O)
 resource "aws_route53_record" "www_zone" {
   zone_id = "Z012345678P2ZFJKIP56R" # your 호스팅 영역 ID, 테라폼으로 생성시 주석 변경
   # zone_id = aws_route53_zone.domain_01.zone_id
@@ -18,10 +20,11 @@ resource "aws_route53_record" "www_zone" {
   records = [aws_lb.ALB.dns_name]
 }
 
-/*
+# root domain (www X)
 resource "aws_route53_record" "A_zone" {
-  zone_id = aws_route53_zone.domain_01.zone_id
-  name    = "example.com" # OR "www.example.com"
+  zone_id = "Z012345678P2ZFJKIP56R" # your 호스팅 영역 ID, 테라폼으로 생성시 주석 변경
+  # zone_id = aws_route53_zone.domain_01.zone_id
+  name    = "example.com"
   type    = "A"           # OR "AAAA"
 
   alias {
@@ -30,4 +33,3 @@ resource "aws_route53_record" "A_zone" {
     evaluate_target_health = true
   }
 }
-*/
