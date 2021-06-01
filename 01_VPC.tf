82e8f127517b893ad6654ecc4aaca92670d5c665
ceool <p1026jh@naver.com>
2021-06-01 15:54:41 +0900

    enable dns hostnames

--- 01_VPC.tf
+++ 01_VPC.tf
@@ -5,7 +5,9 @@ resource "aws_vpc" "VPC_01" {
   cidr_block                       = "10.0.0.0/16" # IPv4 CIDR 블록
   assign_generated_ipv6_cidr_block = "false"       # IPv6 CIDR 블록 여부: true or false
   instance_tenancy                 = "default"     # 테넌시
-
+  enable_dns_hostnames             = "true"
+  enable_dns_support               = "true"
+  
   tags = {
     Name = "VPC_${var.customer}_${var.set_code}" # 태그
   }
